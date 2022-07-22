012a6162835bc2781e3483ca4c3cf1e87de200bb
CrisKLP <71100241+CrisKLP@users.noreply.github.com>
2022-07-22 19:02:08 +0200

    Update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,34 +1,61 @@
-import logoDark from 'assets/logo-dark.svg';
-import logoLight from 'assets/logo-light.svg';
-import logoMobile from 'assets/logo-mobile.svg';
 import iconAddTaskMobile from 'assets/icon-add-task-mobile.svg';
-import iconVerticalEllipsis from 'assets/icon-vertical-ellipsis.svg';
 import iconChevronDown from 'assets/icon-chevron-down.svg';
 import iconChevronUp from 'assets/icon-chevron-up.svg';
+import logoLight from 'assets/logo-light.svg';
+import logoDark from 'assets/logo-dark.svg';
+import logoMobile from 'assets/logo-mobile.svg';
 import Heading from 'components/Heading';
+import Navigation from 'components/Navigation';
+import navData from 'components/Navigation/mock';
+import { useState } from 'react';
+import ContextMenu from 'components/ContextMenu';
 import * as S from './style';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <S.Wrapper>
-      <S.LogoWrapper>
-        <img src={logoMobile} className="mobileLogo" alt="Kanban logo" />
-        <img src={logoDark} className="desktopLogo" alt="Kanban logo" />
-      </S.LogoWrapper>
-      <S.ContentWrapper>
-        <S.Dropdown>
-          <Heading>Platform Launch</Heading>
-          <img src={iconChevronDown} alt="Board list dropdown icon" />
-        </S.Dropdown>
-        <S.NewTaskButton>
-          <img src={iconAddTaskMobile} alt="Add task icon" />
-          <span>+ Add New Task</span>
-        </S.NewTaskButton>
-        <S.ContextMenuButton>
-          <img src={iconVerticalEllipsis} alt="Context menu icon" />
-        </S.ContextMenuButton>
-      </S.ContentWrapper>
-    </S.Wrapper>
+    <>
+      <S.Wrapper>
+        <S.LogoWrapper>
+          <img
+            src={logoMobile}
+            className="mobileLogo"
+            alt="Kanban logo mobile"
+          />
+          <img src={logoDark} className="desktopLogo" alt="Kanban logo" />
+        </S.LogoWrapper>
+        <S.ContentWrapper>
+          <S.NavDropdown onClick={handleMenuOpen}>
+            <Heading>Platform Launch</Heading>
+            <img
+              src={isMenuOpen ? iconChevronUp : iconChevronDown}
+              alt="Board list dropdown icon"
+            />
+          </S.NavDropdown>
+          <Heading as="h2">Platform Launch</Heading>
+          <div>
+            <S.NewTaskButton>
+              <img src={iconAddTaskMobile} alt="Add task icon" />
+              <span>+ Add New Task</span>
+            </S.NewTaskButton>
+            <ContextMenu />
+          </div>
+        </S.ContentWrapper>
+      </S.Wrapper>
+      {isMenuOpen && (
+        <>
+          <Navigation
+            data={navData}
+            onNavClick={() => setIsMenuOpen(false)}
+            mobile
+          />
+          <S.Overlay onClick={() => setIsMenuOpen(false)} />
+        </>
+      )}
+    </>
   );
 }
 
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,21 +1,30 @@
 import Button from 'components/Button';
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0
+  }
+`;
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
-    padding: 0 1.8rem;
+    position: relative;
+    padding: 0 1.6rem;
     display: flex;
     align-items: center;
     height: 6.4rem;
     background-color: ${theme.colors.bg.primary};
+    z-index: 10;
 
     @media (${theme.media.md}) {
       padding: 0 2.4rem;
-      height: 8rem;
+      height: 8.1rem;
+      border-bottom: 1px solid ${theme.colors.navigation.border};
     }
 
     @media (${theme.media.lg}) {
-      height: 9.4rem;
+      height: 9.5rem;
     }
   `};
 `;
@@ -43,6 +52,10 @@ export const LogoWrapper = styled.div`
         display: none;
       }
     }
+
+    @media (${theme.media.lg}) {
+      padding-right: 3.2rem;
+    }
   `};
 `;
 
@@ -50,22 +63,43 @@ export const ContentWrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
     align-items: center;
+    justify-content: space-between;
     width: 100%;
+
+    & > div {
+      display: flex;
+      align-items: center;
+    }
+
+    & > h2 {
+      display: none;
+    }
 
     @media (${theme.media.md}) {
       padding-left: 2.4rem;
+
+      & > h2 {
+        display: block;
+      }
+    }
+
+    @media (${theme.media.lg}) {
+      padding-left: 3.2rem;
     }
   `};
 `;
 
-export const Dropdown = styled.button`
+export const NavDropdown = styled.button`
   ${({ theme }) => css`
     margin-right: 1.6rem;
     display: flex;
     align-items: center;
     gap: 0.9rem;
-    flex: 1;
     cursor: pointer;
+
+    @media (${theme.media.md}) {
+      display: none;
+    }
   `};
 `;
 
@@ -91,6 +125,7 @@ export const NewTaskButton = styled(Button).attrs({
 
 export const ContextMenuButton = styled.button`
   ${({ theme }) => css`
+    position: relative;
     margin-left: 0.6rem;
     margin-right: -1rem;
     padding: 0 1rem;
@@ -98,7 +133,45 @@ export const ContextMenuButton = styled.button`
 
     @media (${theme.media.md}) {
       margin-left: 1.4rem;
-      margin-right: -1rem;
+    }
+
+    @media (${theme.media.lg}) {
+      margin-left: 1.4rem;
+      margin-right: -0.2rem;
+    }
+  `};
+`;
+
+export const ContextMenu = styled.span`
+  ${({ theme }) => css`
+    position: absolute;
+    top: 100%;
+    right: 0;
+    margin-top: 3.6rem;
+    padding: 1.6rem;
+    display: grid;
+    gap: 1.6rem;
+    background-color: ${theme.colors.bg.primary};
+    border-radius: 0.8rem;
+    animation: ${fadeIn} ${theme.transition.fast};
+    z-index: 10;
+  `};
+`;
+
+export const Overlay = styled.div`
+  ${({ theme }) => css`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: ${theme.colors.black};
+    opacity: 0.5;
+    z-index: 0;
+    animation: ${fadeIn} ${theme.transition.fast};
+
+    @media (${theme.media.md}) {
+      display: none;
     }
   `};
 `;
